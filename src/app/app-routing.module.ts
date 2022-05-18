ddd59ba34857dd07e94e9c5d5d9230e2b0b713b4
brianpavon <brian.pavon@hotmail.com>
2022-05-18 16:38:59 -0300

    creo rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActorAltaComponent } from './pages/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './pages/actor-listado/actor-listado.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './pages/pelicula-alta/pelicula-alta.component';
+import { PeliculaListadoComponent } from './pages/pelicula-listado/pelicula-listado.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path: 'bienvenido',redirectTo: 'busqueda',pathMatch: 'full'},
+  {path: 'busqueda', component:BusquedaComponent},
+  {path: 'peliculas/alta',component:PeliculaAltaComponent},
+  {path: 'actor/alta', component:ActorAltaComponent},
+  {path: 'actor/listado', component:ActorListadoComponent},
+  {path:'peliculas/listado',component:PeliculaListadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './pages/bienvenido/bienvenido.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './pages/pelicula-alta/pelicula-alta.component';
+import { ActorAltaComponent } from './pages/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './pages/actor-listado/actor-listado.component';
+import { PeliculaListadoComponent } from './pages/pelicula-listado/pelicula-listado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    BusquedaComponent,
+    PeliculaAltaComponent,
+    ActorAltaComponent,
+    ActorListadoComponent,
+    PeliculaListadoComponent
   ],
   imports: [
     BrowserModule,
