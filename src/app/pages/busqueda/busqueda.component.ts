a2dbe4bd56b5b65651b9572b72a57cd38c024fca
brianpavon <brian.pavon@hotmail.com>
2022-05-18 20:12:59 -0300

    agrego imagenes

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -8,22 +8,26 @@ import { Pelicula } from 'src/app/clases/peliculas/pelicula';
 })
 export class BusquedaComponent implements OnInit {
   listaPeliculasBusqueda : Pelicula[];
-  peliculaParaMostrar: Pelicula;
+  peliculaParaMostrar!: Pelicula;
+  mostrarDetalle:boolean = false
   constructor() { 
     
     this.listaPeliculasBusqueda = [
-      {id:1,nombre:'Top Gun',tipo:'Accion',fechaDeEstreno:'09/10/1986',cantidadDePublico:500000,fotoDePelicula:'urlIMG'},
-      {id:2,nombre:'Iron Man',tipo:'Accion',fechaDeEstreno:'02/05/2008',cantidadDePublico:1500000,fotoDePelicula:'urlIMG'},
-      {id:3,nombre:'Volver al futuro',tipo:'Fantasia',fechaDeEstreno:'26/12/1985',cantidadDePublico:555000,fotoDePelicula:'urlIMG'},
-      {id:4,nombre:'Forrest Gump',tipo:'Drama',fechaDeEstreno:'06/10/1994',cantidadDePublico:200000,fotoDePelicula:'urlIMG'},
+      {id:1,nombre:'Top Gun',tipo:'Accion',fechaDeEstreno:'09/10/1986',cantidadDePublico:500000,fotoDePelicula:'assets/images/topgun.jfif'},
+      {id:2,nombre:'Iron Man',tipo:'Accion',fechaDeEstreno:'02/05/2008',cantidadDePublico:1500000,fotoDePelicula:'assets/images/ironman.jfif'},
+      {id:3,nombre:'Volver al futuro',tipo:'Fantasia',fechaDeEstreno:'26/12/1985',cantidadDePublico:555000,fotoDePelicula:'assets/images/volverfuturo.jfif'},
+      {id:4,nombre:'Forrest Gump',tipo:'Drama',fechaDeEstreno:'06/10/1994',cantidadDePublico:200000,fotoDePelicula:'assets/images/forrestgump.jfif'},
     ]
-    this.peliculaParaMostrar = new Pelicula(0,'','','',0,'');
+    //this.peliculaParaMostrar = new Pelicula(0,'','','',0,'');
   }
 
   ngOnInit(): void {
   }
   peliculaParaDetalle(peli: Pelicula){
     this.peliculaParaMostrar = peli;
-    //console.log(peli);    
+    this.mostrarDetalle = true;
+  }
+  limpiarDetalle(){
+    this.mostrarDetalle = false;
   }
 }
