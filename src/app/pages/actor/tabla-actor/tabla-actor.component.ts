0433b9567db63c9b3d574f20b390a3288151bdf8
brianpavon <brian.pavon@hotmail.com>
2022-05-19 02:24:36 -0300

    cargo actores por servicio

--- src/app/pages/actor/tabla-actor/tabla-actor.component.ts
+++ src/app/pages/actor/tabla-actor/tabla-actor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Actor } from 'src/app/clases/actor';
+import { ActoresService } from 'src/app/services/actores.service';
 
 @Component({
   selector: 'app-tabla-actor',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabla-actor.component.css']
 })
 export class TablaActorComponent implements OnInit {
+  listaActores:Actor[] = [];
+  //listaActores!:Observable<Actor[]>;
+  constructor(private servActores:ActoresService) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    this.cargarActores();
+  }
+
+  cargarActores(){
+    this.servActores.devolverActores().subscribe(
+      actores=>{
+        console.log(actores);
+        this.listaActores=actores;
+        //console.log(this.listaActores);
+      });
   }
 
 }
--- src/app/services/actores.service.ts
+++ src/app/services/actores.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { Actor } from '../clases/actor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActoresService {
-  //nuevoActor!:Actor
-  constructor(private firestore:AngularFirestore) {
-
+  
+  itemsCollection!: AngularFirestoreCollection<Actor>;
+  todosLosActores!: Observable<Actor[]>;
+  constructor(private firestore:AngularFirestore) {    
   }
 
   guardarActor(nuevoActor:Actor){
     this.firestore.collection('actores').add(nuevoActor);
+  }
+  
+  devolverActores(){
+    this.itemsCollection = this.firestore.collection<Actor>('actores');
+    return this.todosLosActores = this.itemsCollection.valueChanges();
   }
 }
